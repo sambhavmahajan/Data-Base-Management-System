dde38fdff215bf2faa0250437a2e0ad641f2f6f0
sambhavmahajan <70747596+sambhavmahajan@users.noreply.github.com>
2023-10-27 22:04:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,177 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+column_names = []
+Data = []
+IndexT = []
+commands_and_syntax = [
+    "create:<column_name1>:<column_name2>:...",
+    "insert:<row_name>:<value1>:<value2>:...",
+    "delete_row:<row_name>",
+    "save:<filename>",
+    "open:<filename>",
+    "show",
+    "sum:<column_name>",
+    "avg:<column_name>",
+    "shape",
+    "plot",
+    "modify:<row_name>:<column_name>:<new_value>",
+    "quit"
+]
+
+
+def main():
+    global IndexT, commands_and_syntax
+    print("Type help to get commands")
+    while True:
+        x = input("Write Command: ")
+        if x == "quit":
+            print("Program Exited successfully")
+            break
+        if x == "help":
+            for t in commands_and_syntax:
+                print(t)
+            continue
+        tkns = tokens(x)
+        command = removespace(tkns[0])
+        tkns.pop(0)
+        for i in range(0,len(tkns)):
+            tkns[i].strip()
+        if command == "create":
+            CreateTable(tkns)
+        elif command == "insert":
+            if tkns[0] in IndexT:
+                print("Error:", tkns[0], "Index already exists")
+            else:
+                IndexT += tkns[0]
+                tkns.pop(0)
+                Insert(tkns)
+        elif command == "delete_row":
+            for s in tkns:
+                DeleteRow(s)
+        elif command == "save":
+            SaveToFile(tkns[0])
+        elif command == "open":
+            OpenFile(tkns[0])
+            Show()
+        elif command == 'show':
+            Show()
+        elif command == 'sum':
+            print("Sum:", Sum(tkns[0]))
+        elif command == 'avg':
+            print("Avg:", Avg(tkns[0]))
+        elif command == 'shape':
+            print(np.array(Data).shape)
+        elif command == "modify":
+            modify(tkns[0], tkns[1], tkns[2])
+        elif command == "plot":
+            Generate_DF().astype(int).plot()
+            plt.show()
+        else:
+            print("Error: Command \'" + command + '\'' + " Does not exist")
+
+    return 0
+
+
+def tokens(li):
+    return removespace(li).split(':')
+
+
+def removespace(s):
+    return s.replace(" ", "")
+
+
+def CreateTable(li):
+    global column_names, Data, IndexT
+    print("Confirm table override (y for yes, any key for no):")
+    t = input()
+    if t == 'y':
+        column_names = li
+        Data = []
+        IndexT = []
+
+    else:
+        print("Table creation canceled")
+
+
+def Insert(li):
+    if len(column_names) != len(li):
+        print("Number of inserted values must match the number of columns in a row")
+        return -1
+    Data.append(li)
+    return 0
+
+
+def GetIndex(arr, Indexname):
+    i = 0
+    for ele in arr:
+        if str(ele) == str(Indexname):
+            return i
+        i += 1
+    return -1
+
+
+def DeleteRow(Row_Name):
+    global IndexT, Data
+    i = GetIndex(IndexT, Row_Name)
+    if i == -1:
+        print("Error: Row with index \'" + Row_Name + "\' does not exist")
+        return
+    IndexT.pop(i)
+    Data.pop(i)
+    print("Row " + '\'' + Row_Name, "deleted successfully")
+    return
+
+
+def Generate_DF():
+    global Data, IndexT
+    df = pd.DataFrame(Data, columns=column_names, index=IndexT)
+    return df
+
+
+def SaveToFile(filename):
+    global column_names, IndexT
+    df = Generate_DF()
+    df.to_csv(filename)
+    print("Saved file to " + filename + " successfully")
+
+
+def OpenFile(filename):
+    global Data, IndexT, column_names
+    df = pd.read_csv(filename, header=0, index_col=0)
+    column_names = df.columns.tolist()
+    IndexT = df.index.tolist()
+    Data = []
+    t = df.values.tolist()
+    for x in t:
+        Data.append(x)
+
+
+def Show():
+    global Data, column_names, IndexT
+    df = Generate_DF()
+    print(df)
+
+
+def modify(i, c, newVal):
+    global IndexT, column_names, Data
+    i0 = GetIndex(IndexT, i)
+    c0 = GetIndex(column_names, c)
+    Data[i0][c0] = newVal
+    return 0
+
+
+def Sum(columnName):
+    r = 0
+    columnindex = column_names.index(columnName)
+    for x in Data:
+        r += int(x[columnindex])
+    return r
+
+
+def Avg(columnName):
+    return Sum(columnName) / len(IndexT)
+
+
+main()
